4bac4bc4121b128993ead481efa26ef2ee132eba
A saivenkat <110588670+saivenkatachala@users.noreply.github.com>
2025-02-04 12:38:11 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies=[
+    {
+        name:'Jhansi',
+        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic minus amet tenetur, repellat iste inventore officiis facere itaque! Ad, voluptatem dignissimos tempora a aut porro deserunt earum obcaecati incidunt quae.',
+        image:'slider-1.png'
+    },
+    {
+        name:'The Warrior',
+        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic minus amet tenetur, repellat iste inventore officiis facere itaque! Ad, voluptatem dignissimos tempora a aut porro deserunt earum obcaecati incidunt quae.',
+        image:'slider-2.png'
+    },
+    {
+        name:'Exposed',
+        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic minus amet tenetur, repellat iste inventore officiis facere itaque! Ad, voluptatem dignissimos tempora a aut porro deserunt earum obcaecati incidunt quae.',
+        image:'slider-3.png'
+    },
+    {
+        name:'Vikram Vedha',
+        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic minus amet tenetur, repellat iste inventore officiis facere itaque! Ad, voluptatem dignissimos tempora a aut porro deserunt earum obcaecati incidunt quae.',
+        image:'slider-4.png'
+    },
+    {
+        name:'Bheemlanayak',
+        des:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic minus amet tenetur, repellat iste inventore officiis facere itaque! Ad, voluptatem dignissimos tempora a aut porro deserunt earum obcaecati incidunt quae.',
+        image:'slider-5.png'
+    }
+
+]
